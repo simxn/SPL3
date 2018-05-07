1468527ee2f84e3b99640e0c75fcd5ca5502f472
simxn <171wita27@o365.htl-leoben.at>
2018-05-07 15:51:48 +0200

    modified simple subprogram method

--- src/Unterprogramme.java
+++ src/Unterprogramme.java
@@ -1,13 +1,41 @@
 
 public class Unterprogramme {
 	public static void main(String[] args) {
-		
-		String text = "Heute ist eine schöner Tag";
+
+		String text = "Heute ist einer schöner Tag";
 		char[] z = text.toCharArray();
-		
-		for(int i = 0; i<z.length; i++) {
-			
+		countChar(z, 'e');
+		countChar(z, 'i');
+		countChar(z, 'ä');
+		System.out.println("--------------------------");
+		System.out.println(countChar2(z, 'e'));
+		System.out.println(countChar2(z, 'i'));
+		System.out.println(countChar2(z, 'ä'));
+
+	}
+
+	public static void countChar(char[] ch, char c) {
+		int counter = 0;
+
+		for (int i = 0; i < ch.length; i++) {
+			if (ch[i] == c) {
+				counter++;
+			}
+		}
+		System.out.println("Anzahl der " + c + "'s im Text: " + counter);
+	}
+
+
+
+	public static int countChar2(char[] ch, char c) {
+		int counter = 0;
+
+		for (int i = 0; i < ch.length; i++) {
+			if (ch[i] == c) {
+				counter++;
+			}
 		}
 		
+		return counter;
 	}
 }
