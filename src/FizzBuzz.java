b4342bfc6eba41466896b57b041f0e055f205ac3
simxn <171wita27@o365.htl-leoben.at>
2018-05-07 14:41:12 +0200

    FizzBuzz v3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,31 +6,32 @@ public class FizzBuzz {
 			FizzBuzz(i);
 		}
 		System.out.println("------");
-		for(int i = 1; i<=100; i++) {
-			FizzerBuzzer(i);
+		for(int i = 0; i<=100; i+=4) {
+			FizzerBuzzer(i, false);
 		}
+		
 		System.out.println();
 		System.out.println("Ende...");
 	}
 	public static void FizzBuzz(int i) {
 		if(i%3 == 0 && i%5 == 0) {
-			System.out.print("FizzBuzz!");
+			System.out.print("("+ i +") FizzBuzz!");
 		} else if(i%3 == 0) {
-			System.out.print("Fizz!");
+			System.out.print("("+ i +") Fizz!");
 		} else if(i%5 == 0) {
-			System.out.print("Buzz!");
+			System.out.print("("+ i +") Buzz!");
 		} else {
-			System.out.print(i);
+			System.out.print("("+i+")");
 		}
 		System.out.println();
 	}
-	public static void FizzerBuzzer(int i) {
-		if(i%4 == 0 && i%7 == 0) {
-			System.out.print("FizzerBuzzer!");
+	public static void FizzerBuzzer(int i, boolean showFizzBuzz) {
+		if(i%4 == 0 && i%7 == 0 && showFizzBuzz == true) {
+			System.out.print("("+ i +") FizzerBuzzer!");
 		} else if(i%4 == 0) {
-			System.out.print("Fizzer!");
+			System.out.print("("+ i +") Fizzer!");
 		} else if(i%7 == 0) {
-			System.out.print("Buzzer!");
+			System.out.print("("+ i +") Buzzer!");
 		} else {
 			System.out.print(i);
 		}
