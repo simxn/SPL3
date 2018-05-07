c1a8388ef1d6f342a61ef65806851ca0faaa859e
simxn <171wita27@o365.htl-leoben.at>
2018-05-07 15:28:10 +0200

    modified temperature average method and added an subprogram example

--- src/TemperaturMittelwert.java
+++ src/TemperaturMittelwert.java
@@ -2,24 +2,39 @@
 public class TemperaturMittelwert {
 	public static void main(String[] args) {
 
-		int unter10grad = 0;
-		int zwischen10und20 = 0;
-
 		int[] temp = new int[1000];
-		// 0-40°C
+
 		for (int i = 0; i < temp.length; i++) {
 			temp[i] = (int) (Math.random() * 41);
 		}
-		// # Temperaturwerte < 10°
 
-		for (int i = 0; i < temp.length; i++) {
-			if (temp[i] <= 10) {
+		String temperatures = getTemperature(temp);
+		System.out.println(temperatures);
+	}
+
+	public static String getTemperature(int[] temperatures) {
+		int unter10grad = 0;
+		int zwischen10und20 = 0;
+		int zwischen20und30 = 0;
+		int ueber30 = 0;
+
+		for (int i = 0; i < temperatures.length; i++) {
+			if (temperatures[i] <= 10) {
 				unter10grad++;
-			} else if (temp[i] >= 10 && temp[i] <= 20) {
+			} else if (temperatures[i] >= 10 && temperatures[i] <= 20) {
 				zwischen10und20++;
+			} else if (temperatures[i] > 20 && temperatures[i] <= 30) {
+				zwischen20und30++;
+			} else if (temperatures[i] > 30) {
+				ueber30++;
 			}
 		}
-		System.out.println("Es wurden " + unter10grad + " Temperaturen unter 10° gemessen.");
-		System.out.println("Es wurden " + zwischen10und20 + " Temperaturen zwischen 10°-20° gemessen.");
+
+		String msg = "Im Messbereich von " + temperatures.length + " wurden " + unter10grad + " Temperaturen unter 10°"
+				+ ", \n " + zwischen10und20 + " Temperaturen zwischen 10°-20°, \n " + zwischen20und30 + " Temperaturen"
+				+ "zwischen 20°-30° \n und " + ueber30 + " Temperaturen über 30° gemessen.";
+
+		return msg;
 	}
+
 }
---
+++ src/Unterprogramme.java
@@ -0,0 +1,13 @@
+
+public class Unterprogramme {
+	public static void main(String[] args) {
+		
+		String text = "Heute ist eine schöner Tag";
+		char[] z = text.toCharArray();
+		
+		for(int i = 0; i<z.length; i++) {
+			
+		}
+		
+	}
+}
