16641b3e663037ec33c13722598dfeed804b51fe
simxn <171wita27@o365.htl-leoben.at>
2018-05-07 16:43:46 +0200

    added a hangman class but didn't finish

---
+++ src/WoerterRaten.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class WoerterRaten {
+	public static void main(String[] args) {
+		
+		String input = JOptionPane.showInputDialog("Please enter a word.");
+		char[] word = input.toCharArray();
+		System.out.println("The searched word has " + word.length + " letters."
+				+ " Begin guessing!");
+		
+		Hangman(word, 'e');
+	}
+	public static String Hangman(char[] word, char searchLetter) {
+		for(int i = 0; i < word.length; i++) {
+			if(word[i] == searchLetter) {
+				System.out.println("There is a/an "+ searchLetter+" on the " + word[i] + " place of the word.");
+			}
+		}
+		
+		
+		
+		
+		return "";
+	}
+}
